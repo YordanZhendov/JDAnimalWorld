95bb7a4703652801e9b3d37b1337b1d6a1a44098
YordanZhendov <YordanNZhendov@gmail.com>
2021-08-13 01:46:19 +0300

    deployment

--- src/main/java/jdanimal/demo/config/ApplicationBeanConfiguration.java
+++ src/main/java/jdanimal/demo/config/ApplicationBeanConfiguration.java
@@ -19,10 +19,4 @@ public class ApplicationBeanConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean(name = "multipartResolver")
-    public CommonsMultipartResolver multipartResolver() {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(100000);
-        return multipartResolver;
-    }
 }
