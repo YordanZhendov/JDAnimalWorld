5b8b9331e13981364feacfe5df3541e89c1f3c34
YordanZhendov <YordanNZhendov@gmail.com>
2021-08-14 21:42:15 +0300

    update controller

--- src/main/java/jdanimal/demo/web/controllers/HomeController.java
+++ src/main/java/jdanimal/demo/web/controllers/HomeController.java
@@ -13,12 +13,6 @@ public class HomeController {
 
     private final AnimalService animalService;
 
-    @GetMapping("/users/login")
-    public String logIn(){
-        return "index";
-    }
-
-
     @GetMapping("/logout")
     public String logOut() {
         return "index";
