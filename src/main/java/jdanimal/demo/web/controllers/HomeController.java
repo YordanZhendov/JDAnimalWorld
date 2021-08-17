cbf1783688e3cf5ba7d866c961918050302ee753
Jordan <68518634+YordanZhendov@users.noreply.github.com>
2021-08-17 15:10:37 +0300

    Update HomeController.java

--- src/main/java/jdanimal/demo/web/controllers/HomeController.java
+++ src/main/java/jdanimal/demo/web/controllers/HomeController.java
@@ -3,21 +3,9 @@ package jdanimal.demo.web.controllers;
 import jdanimal.demo.service.AnimalService;
 import jdanimal.demo.service.views.AnimalViewModel;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;;import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -27,10 +15,10 @@ public class HomeController {
 
     @GetMapping("/users/login")
     public String logIn(HttpServletRequest req){
-        if ((req.getCookies().getValue() != null)) {
+//         if ((req.getCookies().getValue() != null)) {
            
-            return "redirect:/user/home";
-        }
+//             return "redirect:/user/home";
+//         }
         
        return "index";
     }
