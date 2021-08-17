1d99477d5f94a1d4690a363fd44c522e3f0c7190
Jordan <68518634+YordanZhendov@users.noreply.github.com>
2021-08-17 15:59:22 +0300

    Update HomeController.java

--- src/main/java/jdanimal/demo/web/controllers/HomeController.java
+++ src/main/java/jdanimal/demo/web/controllers/HomeController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.util.WebUtils;
 import java.util.List;
 
 @Controller
@@ -15,8 +16,12 @@ public class HomeController {
     private final AnimalService animalService;
 
     @GetMapping("/users/login")
-    public String logIn(){
-       return "index";
+    public String logIn(HttpServletRequest request){
+        Cookie name = WebUtils.getCookie(request, "JSESSIONID");
+        if (name != null) {
+            return "redirect:/user/home"
+        } 
+        return "index";
     }
 
 
