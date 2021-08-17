db3c965ea6574b5c556fdaf59b98aa558e13b6c7
Jordan <68518634+YordanZhendov@users.noreply.github.com>
2021-08-17 16:28:42 +0300

    Update HomeController.java

--- src/main/java/jdanimal/demo/web/controllers/HomeController.java
+++ src/main/java/jdanimal/demo/web/controllers/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
     public String index(HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
         if (cookies[0].equals("JSESSIONID")) {
-            return "redirect:/users/login";
+            return "redirect:/user/home";
         } 
         return "index";
     }
