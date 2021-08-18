e81e8b13c91da5755da3887e685a1add346cb821
Jordan <68518634+YordanZhendov@users.noreply.github.com>
2021-08-18 09:26:14 +0300

    Update HomeController.java

--- src/main/java/jdanimal/demo/web/controllers/HomeController.java
+++ src/main/java/jdanimal/demo/web/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.util.WebUtils;
-import org.springframework.security.core.Authentication
+import org.springframework.security.core.Authentication;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
