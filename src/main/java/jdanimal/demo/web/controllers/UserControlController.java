41b2c4a07e5d59b685cd9d3da67dea2a70e9c09f
Jordan <68518634+YordanZhendov@users.noreply.github.com>
2021-08-20 13:53:36 +0300

    Add files via upload

---
+++ src/main/java/jdanimal/demo/web/controllers/UserControlController.java
@@ -0,0 +1,26 @@
+package jdanimal.demo.web.controllers;
+
+import jdanimal.demo.service.AccessoryService;
+import jdanimal.demo.service.AnimalService;
+import jdanimal.demo.service.views.AccessoryViewModel;
+import jdanimal.demo.service.views.AnimalViewModel;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class UserControlController {
+
+
+    @GetMapping("/user/users-control")
+    public String userControlPage(){
+        return "user-control";
+    }
+
+}
