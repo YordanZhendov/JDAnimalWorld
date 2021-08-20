0c6239a899c67bc6d8a7b97145d3ba2c83953a41
Jordan <68518634+YordanZhendov@users.noreply.github.com>
2021-08-20 15:39:08 +0300

    Update UserControlController.java

--- src/main/java/jdanimal/demo/web/controllers/UserControlController.java
+++ src/main/java/jdanimal/demo/web/controllers/UserControlController.java
@@ -19,12 +19,8 @@ public class UserControlController {
 
 
     @GetMapping("/user/users-control")
-    public String userControlPage(){
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null && auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
-             return "users-control";
-        }
-        return "redirect:/user/home";
+    public String userControlPage(){       
+        return "users-control";
     }
 
 }
