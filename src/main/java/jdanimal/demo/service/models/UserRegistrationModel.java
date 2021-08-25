aef736eb91a53384d72c5c6b6f2bb5893b1c232e
Jordan <68518634+YordanZhendov@users.noreply.github.com>
2021-08-25 10:47:19 +0300

    Update UserRegistrationModel.java

--- src/main/java/jdanimal/demo/service/models/UserRegistrationModel.java
+++ src/main/java/jdanimal/demo/service/models/UserRegistrationModel.java
@@ -25,7 +25,8 @@ public class UserRegistrationModel {
     @Length(min = 5,message = "Password must has minimum 5 symbols!")
     private String confirmPassword;
     @NotBlank()
-    @Email()
+    @Pattern(regexp="(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
+",message="Please enter a correct email address!")  
     private String email;
     @Length(min = 10,max = 10,message = "Enter your 10 digits!")
     private String phoneNumber;
