5c008c466f389a69be6083c0c55d39773c043ab2
Jordan <jordan.zhendov@abv.bg>
2021-11-04 10:21:55 +0200

    test adding beforeeach method

--- src/test/java/jdanimal/demo/repository/UserRepositoryTest.java
+++ src/test/java/jdanimal/demo/repository/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package jdanimal.demo.repository;
 import jdanimal.demo.DemoApplication;
 import jdanimal.demo.data.User;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,61 +21,53 @@ class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
-    //Testing User Entity  ->>>>>>>> NOT BINDING MODELS!!!
-
-    @Test
-    public void registerUser() {
+    @BeforeEach
+    public void setUp(){
         User user = new User();
         user.setUsername("Joro");
-        user.setFullName("Joro Ivanov");
         user.setPassword("12345");
         user.setConfirmPassword("12345");
         user.setEmail("jordan@abv.bg");
         user.setPostcode("4445");
         user.setPhoneNumber("0856258851");
-        user.setCity("Varna");
-        user.setCountry("Bulgaria");
         user.setPolicyAgree(true);
         user.setUserStatus(true);
+
+        User user2 = new User();
+        user2.setUsername("Desi");
+        user2.setPassword("12345");
+        user2.setConfirmPassword("12345");
+        user2.setEmail("desi@abv.bg");
+        user2.setPostcode("5555");
+        user2.setPhoneNumber("0856254851");
+        user2.setPolicyAgree(true);
+        user2.setUserStatus(true);
+
         userRepository.save(user);
+        userRepository.save(user2);
+    }
+    //Testing User Entity  ->>>>>>>> NOT BINDING MODELS!!!
 
-        assertEquals(1,userRepository.count());
-
+    @Test
+    public void registerUser() {
+        assertEquals(2,userRepository.count());
     }
 
     @Test
     public void enterTwoUsersMustThrowException(){
         User user = new User();
         user.setUsername("Joro");
-        user.setFullName("Joro Ivanov");
         user.setPassword("12345");
         user.setConfirmPassword("12345");
         user.setEmail("jordan@abv.bg");
         user.setPostcode("4445");
         user.setPhoneNumber("0856258851");
-        user.setCity("Varna");
-        user.setCountry("Bulgaria");
         user.setPolicyAgree(true);
         user.setUserStatus(true);
 
-        User user2 = new User();
-        user2.setUsername("Joro");
-        user2.setFullName("Joro Ivanov");
-        user2.setPassword("12345");
-        user2.setConfirmPassword("12345");
-        user2.setEmail("jordan@abv.bg");
-        user2.setPostcode("4445");
-        user2.setPhoneNumber("0856258851");
-        user2.setCity("Varna");
-        user2.setCountry("Bulgaria");
-        user2.setPolicyAgree(true);
-        user2.setUserStatus(true);
-
-
         boolean thrown = false;
-            userRepository.save(user);
         try {
-            userRepository.save(user2);
+            userRepository.save(user);
         } catch (Exception e) {
             thrown = true;
         }
@@ -84,26 +77,26 @@ class UserRepositoryTest {
     @Test
     public void enterTwoDifferentUsers(){
         User user = new User();
-        user.setUsername("Joro");
-        user.setFullName("Joro Ivanov");
+        user.setUsername("Dimo");
+        user.setFullName("Dimo Ivanov");
         user.setPassword("12345");
         user.setConfirmPassword("12345");
-        user.setEmail("jordan@abv.bg");
+        user.setEmail("dimoI@abv.bg");
         user.setPostcode("4445");
-        user.setPhoneNumber("0856258851");
+        user.setPhoneNumber("0856577851");
         user.setCity("Varna");
         user.setCountry("Bulgaria");
         user.setPolicyAgree(true);
         user.setUserStatus(true);
 
         User user2 = new User();
-        user2.setUsername("Desi");
-        user2.setFullName("Desi Ivanova");
+        user2.setUsername("Pesho");
+        user2.setFullName("Pesho Ivanov");
         user2.setPassword("12345");
         user2.setConfirmPassword("12345");
-        user2.setEmail("desi@abv.bg");
+        user2.setEmail("peshoi@abv.bg");
         user2.setPostcode("5555");
-        user2.setPhoneNumber("0856254851");
+        user2.setPhoneNumber("0226254851");
         user2.setCity("Varna");
         user2.setCountry("Bulgaria");
         user2.setPolicyAgree(true);
@@ -119,145 +112,34 @@ class UserRepositoryTest {
 
     @Test
     public void checkIfUserExistsByUsername(){
-        User user = new User();
-        user.setUsername("Joro");
-        user.setPassword("12345");
-        user.setConfirmPassword("12345");
-        user.setEmail("jordan@abv.bg");
-        user.setPostcode("4445");
-        user.setPhoneNumber("0856258851");
-        user.setPolicyAgree(true);
-        user.setUserStatus(true);
-
-        User user2 = new User();
-        user2.setUsername("Desi");
-        user2.setPassword("12345");
-        user2.setConfirmPassword("12345");
-        user2.setEmail("desi@abv.bg");
-        user2.setPostcode("5555");
-        user2.setPhoneNumber("0856254851");
-        user2.setPolicyAgree(true);
-        user2.setUserStatus(true);
-
-        userRepository.save(user);
-        userRepository.save(user2);
 
         boolean exists = userRepository.existsByUsername("Desi");
-
         assertTrue(exists);
     }
 
     @Test
     public void checkIfUserExistsByEmail() {
-        User user = new User();
-        user.setUsername("Joro");
-        user.setPassword("12345");
-        user.setConfirmPassword("12345");
-        user.setEmail("jordan@abv.bg");
-        user.setPostcode("4445");
-        user.setPhoneNumber("0856258851");
-        user.setPolicyAgree(true);
-        user.setUserStatus(true);
-
-        User user2 = new User();
-        user2.setUsername("Desi");
-        user2.setPassword("12345");
-        user2.setConfirmPassword("12345");
-        user2.setEmail("desi@abv.bg");
-        user2.setPostcode("5555");
-        user2.setPhoneNumber("0856254851");
-        user2.setPolicyAgree(true);
-        user2.setUserStatus(true);
-
-        userRepository.save(user);
-        userRepository.save(user2);
 
         boolean exists = userRepository.existsByEmail("desi@abv.bg");
-
         assertTrue(exists);
     }
 
     @Test
     public void checkIfUserExistsByPhoneNumber() {
-        User user = new User();
-        user.setUsername("Joro");
-        user.setPassword("12345");
-        user.setConfirmPassword("12345");
-        user.setEmail("jordan@abv.bg");
-        user.setPostcode("4445");
-        user.setPhoneNumber("0856258851");
-        user.setPolicyAgree(true);
-        user.setUserStatus(true);
-
-        User user2 = new User();
-        user2.setUsername("Desi");
-        user2.setPassword("12345");
-        user2.setConfirmPassword("12345");
-        user2.setEmail("desi@abv.bg");
-        user2.setPostcode("5555");
-        user2.setPhoneNumber("0856254851");
-        user2.setPolicyAgree(true);
-        user2.setUserStatus(true);
-
-        userRepository.save(user);
-        userRepository.save(user2);
-
         boolean exists = userRepository.existsAllByPhoneNumber("0856258851");
         assertTrue(exists);
     }
 
     @Test
     public void checkIfCanFindUserByUsername() {
-        User user = new User();
-        user.setUsername("Joro");
-        user.setPassword("12345");
-        user.setConfirmPassword("12345");
-        user.setEmail("jordan@abv.bg");
-        user.setPostcode("4445");
-        user.setPhoneNumber("0856258851");
-        user.setPolicyAgree(true);
-        user.setUserStatus(true);
 
-        User user2 = new User();
-        user2.setUsername("Desi");
-        user2.setPassword("12345");
-        user2.setConfirmPassword("12345");
-        user2.setEmail("desi@abv.bg");
-        user2.setPostcode("5555");
-        user2.setPhoneNumber("0856254851");
-        user2.setPolicyAgree(true);
-        user2.setUserStatus(true);
-
-        userRepository.save(user);
-        userRepository.save(user2);
         User userFoundByUsername = userRepository.findByUsername("Desi");
         assertEquals("desi@abv.bg",userFoundByUsername.getEmail());
     }
 
     @Test
     public void checkIfCanFindUserById() {
-        User user = new User();
-        user.setUsername("Joro");
-        user.setPassword("12345");
-        user.setConfirmPassword("12345");
-        user.setEmail("jordan@abv.bg");
-        user.setPostcode("4445");
-        user.setPhoneNumber("0856258851");
-        user.setPolicyAgree(true);
-        user.setUserStatus(true);
 
-        User user2 = new User();
-        user2.setUsername("Desi");
-        user2.setPassword("12345");
-        user2.setConfirmPassword("12345");
-        user2.setEmail("desi@abv.bg");
-        user2.setPostcode("5555");
-        user2.setPhoneNumber("0856254851");
-        user2.setPolicyAgree(true);
-        user2.setUserStatus(true);
-
-        userRepository.save(user);
-        userRepository.save(user2);
         User userFoundByUsername = userRepository.findByUsername("Desi");
         User userFoundById = userRepository.findById(userFoundByUsername.getId()).orElse(null);
         if(userFoundById != null){
@@ -266,28 +148,7 @@ class UserRepositoryTest {
     }
     @Test
     public void checkIfCanFindUserByUsernameAndPassword() {
-        User user = new User();
-        user.setUsername("Joro");
-        user.setPassword("12345");
-        user.setConfirmPassword("12345");
-        user.setEmail("jordan@abv.bg");
-        user.setPostcode("4445");
-        user.setPhoneNumber("0856258851");
-        user.setPolicyAgree(true);
-        user.setUserStatus(true);
 
-        User user2 = new User();
-        user2.setUsername("Desi");
-        user2.setPassword("12345");
-        user2.setConfirmPassword("12345");
-        user2.setEmail("desi@abv.bg");
-        user2.setPostcode("5555");
-        user2.setPhoneNumber("0856254851");
-        user2.setPolicyAgree(true);
-        user2.setUserStatus(true);
-
-        userRepository.save(user);
-        userRepository.save(user2);
         User userFoundByUsername = userRepository.findByUsername("Desi");
         User userFoundById = userRepository.findByUsernameAndPassword(userFoundByUsername.getUsername(),userFoundByUsername.getPassword()).orElse(null);
         if(userFoundById != null){
@@ -297,28 +158,6 @@ class UserRepositoryTest {
 
     @Test
     public void checkIfCanGetAllUsersInDB() {
-        User user = new User();
-        user.setUsername("Joro");
-        user.setPassword("12345");
-        user.setConfirmPassword("12345");
-        user.setEmail("jordan@abv.bg");
-        user.setPostcode("4445");
-        user.setPhoneNumber("0856258851");
-        user.setPolicyAgree(true);
-        user.setUserStatus(true);
-
-        User user2 = new User();
-        user2.setUsername("Desi");
-        user2.setPassword("12345");
-        user2.setConfirmPassword("12345");
-        user2.setEmail("desi@abv.bg");
-        user2.setPostcode("5555");
-        user2.setPhoneNumber("0856254851");
-        user2.setPolicyAgree(true);
-        user2.setUserStatus(true);
-
-        userRepository.save(user);
-        userRepository.save(user2);
 
         List<User> allUsers = userRepository.getAllUsersInDB();
         assertEquals(userRepository.count(),allUsers.size());
