7d808929ea4b45d948dd8a85a19f057393ab71f6
Jordan <jordan.zhendov@abv.bg>
2021-11-18 14:38:56 +0200

    added API Documentation  + samo messeges after registration

---
+++ src/main/java/jdanimal/demo/web/controllers/APIController.java
@@ -0,0 +1,26 @@
+package jdanimal.demo.web.controllers;
+
+import jdanimal.demo.data.User;
+import jdanimal.demo.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@AllArgsConstructor
+public class APIController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @GetMapping("/api/{username}")
+    public ResponseEntity<User> userByUserName(@PathVariable(name = "username") String username){
+        return new ResponseEntity<>(userRepository.findByUsername(username), HttpStatus.OK);
+    }
+}
