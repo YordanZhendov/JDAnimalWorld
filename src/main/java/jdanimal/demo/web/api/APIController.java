dbc70f166fe236abfbed169fe402800ab6c10d51
Jordan <jordan.zhendov@abv.bg>
2021-11-30 11:02:20 +0200

    API

--- src/main/java/jdanimal/demo/web/api/APIController.java
+++ src/main/java/jdanimal/demo/web/api/APIController.java
@@ -1,4 +1,4 @@
-package jdanimal.demo.web.controllers;
+package jdanimal.demo.web.api;
 
 import jdanimal.demo.data.User;
 import jdanimal.demo.repository.UserRepository;
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class APIController {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-
+    @CrossOrigin
     @GetMapping("/api/{username}")
     public ResponseEntity<User> userByUserName(@PathVariable(name = "username") String username){
         return new ResponseEntity<>(userRepository.findByUsername(username), HttpStatus.OK);
