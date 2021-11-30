3149333f0fd18853a7137e206102ebf27cb7339e
Jordan <jordan.zhendov@abv.bg>
2021-11-30 14:08:54 +0200

    API

--- src/main/java/jdanimal/demo/web/api/APIController.java
+++ src/main/java/jdanimal/demo/web/api/APIController.java
@@ -1,6 +1,12 @@
 package jdanimal.demo.web.api;
 
+import jdanimal.demo.data.Accessory;
+import jdanimal.demo.data.Animal;
+import jdanimal.demo.data.Store;
 import jdanimal.demo.data.User;
+import jdanimal.demo.repository.AccessoryRepository;
+import jdanimal.demo.repository.AnimalRepository;
+import jdanimal.demo.repository.StoreRepository;
 import jdanimal.demo.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,17 +17,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
+
+@CrossOrigin("http://localhost:3000")
 @RestController
 @AllArgsConstructor
 public class APIController {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final AnimalRepository animalRepository;
+    private final AccessoryRepository accessoryRepository;
+    private final StoreRepository storeRepository;
 
-    @CrossOrigin(origins = "https://yordanzhendov.github.io")
     @GetMapping("/api/{username}")
     public ResponseEntity<User> userByUserName(@PathVariable(name = "username") String username){
         return new ResponseEntity<>(userRepository.findByUsername(username), HttpStatus.OK);
     }
+
+    @GetMapping("/api/allusers")
+    public ResponseEntity<List<User>> allUsers(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/allanimals")
+    public ResponseEntity<List<Animal>> allAnimals(){
+        return new ResponseEntity<>(animalRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/allaccessories")
+    public ResponseEntity<List<Accessory>> allAccessories(){
+        return new ResponseEntity<>(accessoryRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/stores")
+    public ResponseEntity<List<Store>> allStores(){
+        return new ResponseEntity<>(storeRepository.findAll(), HttpStatus.OK);
+    }
+
 }
